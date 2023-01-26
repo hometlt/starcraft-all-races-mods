e8308963b1ef19bdde7e41d3bf63c5a773311b3c
hometlt <visceroi@gmail.com>
2023-01-26 15:47:31 +0400

    MP mods, Core mods

--- .scripts/use core.bat
+++ .scripts/use core.bat
@@ -1,4 +1,5 @@
 RMDIR  /s /q "../../ARC.SC2Mod"
 XCOPY  "ARC.SC2Mod" "../../ARC.SC2Mod/*"
 XCOPY  /e /i "ARC.SC2Mod/enUS.SC2Data" "../../ARC.SC2Mod/enUS.SC2Data/*"
-XCOPY  "ARC.SC2Mod/versions/COOP-Core" "../../ARC.SC2Mod/*"
+XCOPY  "ARC.SC2Mod/versions/Core" "../../ARC.SC2Mod/*"
+pause
