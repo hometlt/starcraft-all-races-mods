1be0a66bca418cd2688b33ac5e8f09389a769e36
hometlt <visceroi@gmail.com>
2023-04-03 13:17:19 +0200

    nexus, renaming

---
+++ scripts/combine all races coop addon dependencies (make ARM mod).bat
@@ -0,0 +1,3 @@
+::this operation might need >2Gb of Memory sometimes
+node --max_old_space_size=4000 node_modules/sc-parser/run --read ../../all-races-assets/{Scion,UED,Hybrids,Dragons,UPL,UPLCampaign,Talon},../factions/{Scion,Hybrids,UED,Dragons,UPL,UPLCampaign,UPLBalance,Talon} --write ../../ARM.SC2Mod
+pause
