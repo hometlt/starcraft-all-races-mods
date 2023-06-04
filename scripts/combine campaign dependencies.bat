feff10c0ca7a05171ccede2876ce293604b40b9e
hometlt <visceroi@gmail.com>
2023-06-04 02:08:45 +0200

    ...

--- scripts/combine campaign dependencies.bat
+++ scripts/combine campaign dependencies.bat
@@ -1,3 +1,3 @@
 ::this operation might need >2Gb of Memory sometimes
-node --max_old_space_size=4000 node_modules/sc-parser/run --read ../built-in/{LibertyCampaign,Swarm,SwarmCampaign,Void,VoidCampaign,VoidMulti},../dependencies/LIB --write ../built-in/CampaignCombined.SC2Mod
+node --max_old_space_size=800 node_modules/sc-parser/run --read ../built-in/{LibertyCampaign,Swarm,SwarmCampaign,Void,VoidCampaign,VoidMulti},../dependencies/LIB --write ../built-in/CampaignCombined.SC2Mod
 pause
